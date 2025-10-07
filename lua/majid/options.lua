e92abda115d5e3a4399216eda0426fd6b6686bd4
Majid <majid@Majids-MacBook-Air.local>
2025-10-07 03:40:58 +0300

    add nvim-lspconfig, readme

--- lua/majid/options.lua
+++ lua/majid/options.lua
@@ -1,23 +1,19 @@
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.signcolumn = "yes"
-vim.o.wrap = false
-vim.o.tabstop = 4
-vim.o.swapfile = false
-vim.o.winborder = "rounded"
+local opt = vim.opt
+opt.number = true
+opt.relativenumber = true
+opt.signcolumn = "yes"
+opt.wrap = false
+opt.tabstop = 4
+opt.swapfile = false
+opt.winborder = "rounded"
 vim.g.mapleader = " "
 
 vim.diagnostic.config({
-	signs = false, -- show in gutter
-	underline = true, 
+	signs = false,
+	underline = true,
 	update_in_insert = true,
 	severity_sort = true,
 	virtual_text = {
 		severity = { min = vim.diagnostic.severity.WARN },
 	},
 })
-
-
-
-
-
